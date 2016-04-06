baa53133d582daa36198828bcfc32645388cd207
Thulisile <thulisilethukwana@gmail.com>
2016-04-06 12:12:15 +0200

    Initial checkin of Employee Hibernate project

---
+++ src/com/employee/Certificate.java
@@ -0,0 +1,12 @@
+package com.employee;
+
+public class Certificate {
+
+	public static void main(String[] args)
+	{
+		Employee emp = new Employee();
+		
+		System.out.println(emp.toString());
+	}
+
+}
